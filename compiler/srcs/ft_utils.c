1ae878d0296891df4c3b483102bd3f16f26c249a
ggerardy <liftchampion@gmail.com>
2019-05-24 12:09:17 +0300

    parsing beginning

--- compiler/srcs/ft_utils.c
+++ compiler/srcs/ft_utils.c
@@ -1,18 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                            ::: ::::::::    */
-/*    parser.c                                           :+: :+: :+:          */
+/*    ft_utils.c                                         :+: :+: :+:          */
 /*                                                             +:+ +:+ +:+    */
 /*    By: ggerardy <marvin@42.fr>                    +#+ +:+ +#+              */
 /*                                                         +#+#+#+#+#+ +#+    */
-/*    Created: 2019/05/24 10:42:03 by ggerardy          #+# #+#               */
-/*    Updated: 2019/05/24 10:42:03 by ggerardy         ### ########.fr        */
+/*    Created: 2019/05/24 12:02:09 by ggerardy          #+# #+#               */
+/*    Updated: 2019/05/24 12:02:09 by ggerardy         ### ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
 
-int 	ft_parse_champ(t_champ *champ) {
-
+int 		ft_free_champ(t_champ **champ, int ret)
+{
+	if (!champ || !*champ)
+	{
+		return (ret);
+	}
+	ft_free_vector(&(*champ)->tokens);
+	free(*champ);
+	*champ = 0;
+	return (ret);
 }
 
+t_champ		*ft_make_champ()
+{
+	t_champ *champ;
+
+	if (!(champ = (t_champ*)ft_memalloc(sizeof(t_champ)))) {
+		return (0);
+	}
+	if (!(champ->tokens = ft_make_vector_free()))
+}
