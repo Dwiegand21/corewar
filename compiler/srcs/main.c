b33f8567d8a702e8f99980f3a35c646c86d00d30
ggerardy <liftchampion@gmail.com>
2019-06-08 19:04:46 +0300

    Comparator modified

--- compiler/srcs/main.c
+++ compiler/srcs/main.c
@@ -62,21 +62,6 @@ char *ft_upd_name(char *name, char *postfix)
 
 int main(int ac, char **av)
 {
-
-//	void *ptr = ft_memalloc(100);
-//	t_token_type type = D;
-//
-//
-//	ft_printf("%#llB\n", ptr);
-//
-//	size_t p = (size_t)tokenize(type, ptr);
-//
-//	ft_printf("%#B\n", 84);
-//	ft_printf("%#B\n\n\n", 80);
-//
-//	ft_printf("%#llB\n", GET_DATA(p));
-//	ft_printf("%#hhB\n", GET_TYPE(p));
-
 	if (ac != 2)
 		return (0);
 
@@ -89,64 +74,33 @@ int main(int ac, char **av)
 	if (champ->error_count)
 		return (0);
 
-//	int fd = open("toto.cor", O_RDONLY);
-//	char *ln;
-//	int res;
-//	t_string *ref;
-//	t_string *my = champ->res;
-//	size_t i = 0;
-//
-//	while (i < my->len)
-//	{
-//		ref = ft_gnl_bin(fd, &ln, 10000, &res);
-//		if (!res)
-//		{
-//			ft_printf("{Blue}Ref ended{eof}\n");
-//			return (0);
-//		}
-//		ft_string_push_back(&ref, '\n');
-//		size_t j = 0;
-//		while (j < ref->len)
-//		{
-//			ft_printf("Different chars ref:{Yellow}%5d{eof} my:{Yellow}%5d{eof} in pos %5d |%c\n",
-//					  ref->data[i], my->data[i], i, ref->data[i] == my->data[i] ? '+' : '-');
-//			++j;
-//			++i;
-//		}
-//		ft_printf("{Red}Ref ended{eof}\n");
-//	}
-//	ft_printf("{Red}My ended{eof}\n");
-
 
 	char *new_name = ft_upd_name(av[1], "cor");
 //	char *last_slash = ft_rstrchr(new_name, '/');
 //	new_name = last_slash + 1;
 
 
-	int fd = open(new_name, O_CREAT | O_TRUNC | O_WRONLY, 0666);
+	///int fd = open(new_name, O_CREAT | O_TRUNC | O_WRONLY, 0666);
 
-//	t_string *ref = ft_readall("master_of_puppets.cor");
-//	t_string *my = champ->res;
-//	size_t i = 0;
-//	while (i < my->len && i < ref->len)
-//	{
-//		ft_printf("Different chars ref:{Yellow}%5d{eof} my:{Yellow}%5d{eof} in pos %5d |%c\n",
-//			ref->data[i], my->data[i], i, ref->data[i] == my->data[i] ? '+' : '-');
-//		i++;
-//	}
-//	if (i == ref->len)
-//		ft_printf("{Red}Ref ended{eof}\n");
-//	if (i == my->len)
-//		ft_printf("{Red}My ended{eof}\n");
+	t_string *ref = ft_readall(new_name);
+	t_string *my = champ->res;
+	size_t i = 0;
+	while (i < my->len && i < ref->len)
+	{
+		ft_printf("Different chars ref:{Yellow}%.2x{eof} my:{Yellow}%.2x{eof} in pos %5d |%s\n",
+				  (u_char)ref->data[i], (u_char)my->data[i], i,
+			ref->data[i] == my->data[i] ?
+			"\x1B[32m+\x1B[0m" : "\x1B[1m\x1B[31m-\x1B[0m err");
+		i++;
+	}
+	if (i == ref->len)
+		ft_printf("{Red}Ref ended{eof}\n");
+	if (i == my->len)
+		ft_printf("{Red}My ended{eof}\n");
 
-	write(fd, champ->res->data, champ->res->len);
+	///write(fd, champ->res->data, champ->res->len);
 
 	ft_free_champ(&champ, 0);
-
-
-
-	//printf("<%c>%d\n%s", c, c,BACKSLASH_LITERALS);
-
 
 	return 0;
 }
