dc837cd37f410c7b3915702a7835fe799c455fb0
Axtazy <rudenkodan2010@yandex.ru>
2019-06-18 04:27:41 +0300

    new subm, update glfw

--- virtual_machine/visualization/includes/visualization.h
+++ virtual_machine/visualization/includes/visualization.h
@@ -4,12 +4,17 @@
 
 # include <stdio.h>
 # include <stdlib.h>
-# include <stdbool.h>
+//# include <stdbool.h>
+//# include <math.h>
 
 # include "GL/glew.h"
 # include "GLFW/glfw3.h"
-//# include "../external/soil-1.16/inc/SOIL/SOIL.h"
-# include "SOIL/SOIL.h"
+//# include "SOIL/SOIL.h"
+//# include <GL/glu.h>
+//# include "GL/freeglut.h"
+
+# define MAIN_DIR		"/home/axtazy/Projects/21-school/C_projects/corewar/\
+virtual_machine/visualization/"
 
 typedef struct s_visual t_visual;
 
@@ -17,5 +22,7 @@ struct s_visual
 {
     GLFWwindow *window;
 };
+
+void		myApplication(t_visual *main);
 
 #endif //VISUALIZATION_VISUALIZATION_H
