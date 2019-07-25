36e575688876bdb4934b8e397bce700138dfc7fd
Axtazy <rudenkodan2010@yandex.ru>
2019-07-25 19:22:56 +0300

    wecd

--- virtual_machine/visualization/external/AddLibraries.cmake
+++ virtual_machine/visualization/external/AddLibraries.cmake
@@ -1,6 +1,8 @@
 
 cmake_minimum_required(VERSION 3.14)
 
+
+
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/external/cmake_modules/sdl2)
 
 #find_package(OpenGL REQUIRED)
@@ -13,7 +15,7 @@ list(APPEND CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/external/cmake_modules/sdl2)
 
 find_package(SDL2)
 
-if(SDL2_FOUND)
+if( SDL2_FOUND )
     set( SDL2_INC
             SDL2::Main
             SDL2::Image
@@ -29,7 +31,12 @@ if(SDL2_FOUND)
             OpenGL::GL
             )
 else()
-    message( STATUS "library not found" )
+    set( LIBRARY_REPO
+
+            )
+    if ( NOT EXISTS "${CMAKE_SOURCE_DIR}/external/SDL-2.0.mirror5")
+        message( STATUS " >>>> file was found")
+    endif()
     add_subdirectory("${CMAKE_SOURCE_DIR}/external/SDL-2.0.mirror")
 #    add_library(SDL2::Main2 UNKNOWN IMPORTED)
 #    set_target_properties(SDL2::Main2 PROPERTIES
