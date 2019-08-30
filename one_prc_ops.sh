cf00c11a166f0b2b73dbf3032b01bb5ae89b986b
ggerardy <liftchampion@gmail.com>
2019-08-30 19:54:38 +0300

    Rm trash

--- one_prc_ops.sh
+++
@@ -1,8 +0,0 @@
-CW="./subject/corewar"
-CHAMPS="./compiler/champs_bins/echo.cor"
-av=("$@")
-prc=${av[0]}
-reg=${av[1]}
-
-${CW} -v 4 ${CHAMPS} | tr -s ' ' | tr -d '\n' | sed 's/P /#P /g' | \
-  tr '#' '\n' | grep "P ${prc}" | grep "r${reg} "
--- virtual_machine/debug/executable/champs_bins/Define.mk
+++
@@ -1,6 +0,0 @@
-
-export Path1 = "Hello!"
-
-Path2 := $(DEP_PATH)
-Path3 := $(shell cd `$(find ../corewar -name 'includes')` | pwd)
-#Path3 := $(shell find ../corewar -name 'includes')
